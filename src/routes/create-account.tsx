ac5518e1311a79492d8d263a73f3c32c036b6301
jina95 <spzn02@gmail.com>
2024-01-12 22:41:27 +0000

    feature error handling for creating account and style

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,76 +1,138 @@
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { auth } from "../firebase"
 import { Link, useNavigate } from "react-router-dom"
 import { FirebaseError } from "firebase/app"
-import { Border, Error, Form, Input, SocialWrapper, Switcher, SwitcherWrapper, Title, Wrapper } from "../components/auth-components"
+import {
+  Border,
+  Error,
+  FlexWrapper,
+  Form,
+  Input,
+  SocialWrapper,
+  Switcher,
+  SwitcherWrapper,
+  Title,
+  Wrapper,
+} from "../components/auth-components"
 import GithubButton from "../components/github-btn"
 import GoogleButton from "../components/google-btn"
+import { emailRegex, passwordRegex } from "../utils/regex"
+import { setErrorMsg } from "../utils/error-auth-firebase"
 
-// const errors = {
-//   "auth/email-already-in-use": "That email already exists."
-// }
-
-
-export default function CreateAccount(){
+export default function CreateAccount() {
   const navigate = useNavigate()
   const [isLoading, setLoading] = useState(false)
+  const [isReady, setReady] = useState(false)
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [error, setError] = useState("")
+
+  useEffect(() => {
+    if (!name || !email || !password) return
+
+    if (emailRegex(email) && passwordRegex(password)) {
+      setReady(true)
+    } else setReady(false)
+  }, [name, email, password])
+
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { target: {name, value} } = e
-    if(name === "name" ){
+    const {
+      target: { name, value },
+    } = e
+    if (name === "name") {
       setName(value)
-    } else if(name === "password"){
+    } else if (name === "password") {
       setPassword(value)
     } else if (name === "email") {
       setEmail(value)
     }
   }
-  const onSubmit = async(e:React.FormEvent<HTMLFormElement>) => {
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     setError("")
-    if(isLoading || name === "" || email === "" || password === "") return
+    if (
+      !setReady ||
+      isLoading ||
+      name === "" ||
+      email === "" ||
+      password === ""
+    )
+      return
     try {
       setLoading(true)
-      const credentials = await createUserWithEmailAndPassword(auth, email, password)
+      const credentials = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
       await updateProfile(credentials.user, {
-        displayName: name
+        displayName: name,
       })
       navigate("/")
     } catch (e) {
-      if(e instanceof FirebaseError){
-        setError(e.message)
+      if (e instanceof FirebaseError) {
+        const errorMsg = setErrorMsg(e.code)
+        setError(errorMsg)
       }
-    }
-    finally{
+    } finally {
       setLoading(false)
     }
-    console.log("onSubmit", name, email, password)
   }
-  return<Wrapper>
-    <Title>Join 🐳</Title>
-    <Form onSubmit={onSubmit}>
-      <Input onChange={onChange} name="name" value={name} placeholder="Name" type="text" required/>
-      <Input onChange={onChange} name="email" value={email} placeholder="Email" type="email" required/>
-      <Input onChange={onChange} name="password" value={password} placeholder="Password" type="password" required/>
-      <Input type="submit" value={isLoading ? "Loading..." : "Create Account"} />
-    </Form>
-    {error !== "" ? <Error>{error}</Error> : null}
-    <SwitcherWrapper>
-      <Border/>
-      <Switcher>
-        Already have an account? <Link to="/login">Log in &rarr;</Link>
-      </Switcher>
-      <Border/>
-    </SwitcherWrapper>
-    <Switcher>
-    </Switcher>
-    <SocialWrapper>
-      <GithubButton />
-      <GoogleButton />
-    </SocialWrapper>
-  </Wrapper>
-} 
+  return (
+    <Wrapper>
+      <FlexWrapper>
+        <Title>Join &nbsp;</Title>
+      </FlexWrapper>
+      <FlexWrapper>
+        <Form onSubmit={onSubmit}>
+          <Input
+            onChange={onChange}
+            name='name'
+            value={name}
+            placeholder='Name'
+            type='text'
+            required
+          />
+          <Input
+            onChange={onChange}
+            name='email'
+            value={email}
+            placeholder='Email'
+            type='email'
+            required
+          />
+          <Input
+            onChange={onChange}
+            name='password'
+            value={password}
+            placeholder='Password'
+            type='password'
+            required
+          />
+          <Input
+            type='submit'
+            className={isReady ? "isReady" : ""}
+            disabled={isLoading || !isReady}
+            value={isLoading ? "Loading..." : "Create Account"}
+          />
+        </Form>
+        <Error>{error}</Error>
+        <SwitcherWrapper>
+          <Border />
+          <Switcher>
+            Already have an account? &nbsp;
+            <Link to='/login'> Log in</Link>
+          </Switcher>
+          <Border />
+        </SwitcherWrapper>
+        <Switcher></Switcher>
+        <SocialWrapper>
+          <GithubButton />
+          <GoogleButton />
+        </SocialWrapper>
+      </FlexWrapper>
+    </Wrapper>
+  )
+}
