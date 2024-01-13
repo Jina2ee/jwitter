59442f2cb09f4b4f17f22ad030cfd35a9c088c4a
jina95 <spzn02@gmail.com>
2024-01-13 20:57:27 +0000

    feature preview uploaded img, delete preview button

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,5 +1,5 @@
 import { addDoc, collection, updateDoc } from "firebase/firestore"
-import { auth, db, storage } from '../firebase'
+import { auth, db, storage } from "../firebase"
 import { useState } from "react"
 import { styled } from "styled-components"
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage"
@@ -7,7 +7,7 @@ import { getDownloadURL, ref, uploadBytes } from "firebase/storage"
 const Form = styled.form`
   display: flex;
   flex-direction: column;
-  gap: 10px; 
+  gap: 10px;
 `
 const TextArea = styled.textarea`
   border: 2px solid white;
@@ -18,14 +18,15 @@ const TextArea = styled.textarea`
   background-color: black;
   width: 100%;
   resize: none;
-  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   &::placeholder {
     font-size: 16px;
-  };
+  }
   &:focus {
     outline: none;
     border-color: #1d9bf0;
-  };
+  }
 `
 const AttachFileButton = styled.label`
   padding: 10px 0px;
@@ -40,69 +41,136 @@ const AttachFileButton = styled.label`
 const AttachFileInput = styled.input`
   display: none;
 `
+
+const PreviewWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  text-align: center;
+`
+const PreviewImg = styled.img`
+  width: 100%;
+  margin: 0 auto;
+  @media (min-width: 768px) {
+    width: 50%;
+  }
+`
+const DeletePreviewButton = styled.button`
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 2rem;
+  width: 2rem;
+  padding: 0;
+  margin: 0;
+  background-color: transparent;
+  border: none;
+`
 const SubmitButton = styled.input`
-  background-color: #1d9bf0;;
+  background-color: #1d9bf0;
   color: white;
   border: none;
   padding: 10px 0px;
   border-radius: 20px;
   font-size: 16px;
   cursor: pointer;
-  &:hover, &:active {
+  &:hover,
+  &:active {
     opacity: 0.9;
   }
 `
 
-export default function PostTweetForm(){
-  const [ isLoading, setLoading ] = useState(false)
-  const [tweet, setTweet ] = useState("")
-  const [file, setFile ] = useState<File|null>(null)
-  
-  const onChange = (e:React.ChangeEvent<HTMLTextAreaElement>) => {
+export default function PostTweetForm() {
+  const [isLoading, setLoading] = useState(false)
+  const [tweet, setTweet] = useState("")
+  const [file, setFile] = useState<File | null>(null)
+  const [imgUrl, setImgUrl] = useState("")
+
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setTweet(e.target.value)
   }
-  const onFileChange = (e:React.ChangeEvent<HTMLInputElement>) => {
-    const { files } = e.target 
-    if(files && files.length === 1){
+  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log("files 작동하는가?")
+    const { files } = e.target
+    if (files && files.length === 1) {
       setFile(files[0])
+      const url = URL.createObjectURL(files[0])
+      setImgUrl(url)
     }
   }
 
-  const onSubmit = async(e:React.FormEvent<HTMLFormElement>) => {
+  const onDeletePreview = () => {
+    setFile(null)
+    setImgUrl("")
+  }
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     const user = auth.currentUser
-    if(!user || isLoading || tweet === "" || tweet.length > 180) return;
+    if (!user || isLoading || tweet === "" || tweet.length > 180) return
 
     try {
       setLoading(true)
       const doc = await addDoc(collection(db, "tweets"), {
-        tweet, 
+        tweet,
         createdAt: Date.now(),
         userName: user.displayName || "Anonymous",
-        userId: user.uid
+        userId: user.uid,
       })
-      if(file){
+      if (file) {
         const locationRef = ref(storage, `tweets/${user.uid}/${doc.id}`)
         const result = await uploadBytes(locationRef, file)
         const url = await getDownloadURL(result.ref)
         await updateDoc(doc, {
-          photo: url
+          photo: url,
         })
       }
       setTweet("")
       setFile(null)
+      setImgUrl("")
     } catch (error) {
       console.error(error)
-    }finally{
+    } finally {
       setLoading(false)
     }
   }
   return (
     <Form onSubmit={onSubmit}>
-      <TextArea required rows={5} maxLength={180} onChange={onChange} value={tweet} placeholder="What is happening?"/>
-      <AttachFileButton htmlFor="file">{file ? "Photo added ✓" : "Add Photo"}</AttachFileButton>
-      <AttachFileInput onChange={onFileChange} type="file" id="file" accept="image/*" />
-      <SubmitButton type="submit" value={isLoading ? "Posting.." : "Post tweet"} />
+      <TextArea
+        required
+        rows={5}
+        maxLength={180}
+        onChange={onChange}
+        value={tweet}
+        placeholder='What is happening?'
+      />
+      <AttachFileButton htmlFor='file'>
+        {file ? "Photo added ✓" : "Add Photo"}
+      </AttachFileButton>
+      <AttachFileInput
+        onChange={onFileChange}
+        type='file'
+        id='file'
+        accept='image/*'
+      />
+      {imgUrl ? (
+        <PreviewWrapper>
+          <PreviewImg src={imgUrl} />
+          <DeletePreviewButton onClick={onDeletePreview}>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 20 20'
+              fill='#1d9bf0'
+              className='w-5 h-5'
+            >
+              <path d='M6.28 5.22a.75.75 0 0 0-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 1 0 1.06 1.06L10 11.06l3.72 3.72a.75.75 0 1 0 1.06-1.06L11.06 10l3.72-3.72a.75.75 0 0 0-1.06-1.06L10 8.94 6.28 5.22Z' />
+            </svg>
+          </DeletePreviewButton>
+        </PreviewWrapper>
+      ) : null}
+      <SubmitButton
+        type='submit'
+        value={isLoading ? "Posting.." : "Post tweet"}
+      />
     </Form>
   )
 }
