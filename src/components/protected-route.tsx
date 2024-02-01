38ef7aac210879df1d6cfc7bcd28e21c87abc675
jina95 <spzn02@gmail.com>
2024-02-01 13:19:08 +0000

    delete logging in protected-route

--- src/components/protected-route.tsx
+++ src/components/protected-route.tsx
@@ -1,11 +1,14 @@
 import { Navigate } from "react-router-dom"
 import { auth } from "../firebase"
 
-export default function ProtectedRoute({children}: { children: React.ReactElement}){
+export default function ProtectedRoute({
+  children,
+}: {
+  children: React.ReactElement
+}) {
   const user = auth.currentUser
-  console.log("user", user)
-  if(user === null){
-    return <Navigate to="/login" />
+  if (user === null) {
+    return <Navigate to='/login' />
   }
   return children
 }
