0b4612b5e304dba052f54bc3397962965946d407
Jina2ee <jina2ee1011@gmail.com>
2024-02-11 21:47:32 +0000

    Update css in Loading-screen.tsx

--- src/components/loading-screen.tsx
+++ src/components/loading-screen.tsx
@@ -1,4 +1,4 @@
-import { styled } from "styled-components"
+import { styled, keyframes } from "styled-components"
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -7,10 +7,33 @@ const Wrapper = styled.div`
   align-items: center;
 `
 
-const Text = styled.span`
-  font-size: 24px;
+const rotate360 = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
 `
 
-export default function LoadingScreen(){
-  return <Wrapper><Text>Loading...</Text></Wrapper>
+const Spinner = styled.div`
+  animation: ${rotate360} 1s linear infinite;
+  transform: translateZ(0);
+
+  border-top: 2px solid grey;
+  border-right: 2px solid grey;
+  border-bottom: 2px solid grey;
+  border-left: 4px solid black;
+  background: transparent;
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+`
+
+export default function LoadingScreen() {
+  return (
+    <Wrapper>
+      <Spinner />
+    </Wrapper>
+  )
 }
